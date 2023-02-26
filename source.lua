518f0a996f405d25e65a673d27f00a2d0496983d
AnAvaragelilmemer <116945939+AnAvaragelilmemer@users.noreply.github.com>
2023-02-26 14:43:10 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,5 +1,6 @@
-local loadtime = os.time()
+
 repeat task.wait() until game:IsLoaded() and game.ContentProvider.RequestQueueSize == 0;
+local loadtime = os.time()
 local lplr = game.Players.LocalPlayer
 local chlplr = lplr.Character.Humanoid
 local name = lplr.Name
